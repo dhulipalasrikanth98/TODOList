8753847196f557a9a13e568f08f6078db76db953
dhulipalasrikanth98 <dhulipalasrikanth98@outlook.com>
2023-08-31 19:51:08 +0530

    project setup

---
+++ TODO-List/src/test/java/com/example/todo/TODO/List/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo.TODO.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
