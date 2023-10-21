65e16f61bd0627ba9f26b9b279277781d2387055
dhulipalasrikanth98 <dhulipalasrikanth98@outlook.com>
2023-10-22 01:09:09 +0530

    Added Authentication for the api

--- TODO-List/src/main/java/com/example/todo/TODO/List/TodoListApplication.java
+++ TODO-List/src/main/java/com/example/todo/TODO/List/TodoListApplication.java
@@ -2,19 +2,13 @@ package com.example.todo.TODO.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @SpringBootApplication
 public class TodoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
-	@Bean
+/*	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.anyRequest().authenticated()
@@ -22,7 +16,9 @@ public class TodoListApplication {
 				.httpBasic()
 				.and().csrf().disable();
 		return http.build();
-	}
+		}
+ */
+
 
 }
 
